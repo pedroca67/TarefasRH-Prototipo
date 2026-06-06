3ed2fc93894a4e807479d1afc19e8d06a59b1094
PEDRO-PC <pedrorafael4005@gmail.com>
2026-06-05 22:55:07 -0300

    fix: altera fuso horário para America/Fortaleza (São Luís)

--- back/src/main/java/com/potiguar/tarefasrh/TarefasrhApplication.java
+++ back/src/main/java/com/potiguar/tarefasrh/TarefasrhApplication.java
@@ -2,9 +2,16 @@ package com.potiguar.tarefasrh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class TarefasrhApplication {
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Fortaleza"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TarefasrhApplication.class, args);
 
